e28a8e4d505f84a1e5997fef777e27c3c713b714
Tanmesh Joshi <tanmeshj2002@gamil.com>
2025-11-12 12:20:34 +0530

    practice 2

--- mini projects/Guess_number.py
+++ mini projects/Guess_number.py
@@ -1,6 +1,17 @@
 import random
 
 secret_number = random.randint(1,100)
-print(secret_number)
+attempt = 0
 
-num = input("Give me the number between 1 to 100 :")
+
+while True:
+    attempt += 1
+    num = int(input("Give me the number between 1 to 100 :"))
+    if num != secret_number:
+        if num<secret_number:
+            print("Too low ! Try again.")
+        else: 
+            print("Too high ! Try again.")
+    else:
+        print(f"Congratulation ! you guessed it in {attempt} attempts.")
+        break
