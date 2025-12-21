a42a2f56332f8de712db3fc1c2a329532c19c36e
Tanmesh Joshi <tanmeshj2002@gamil.com>
2025-12-21 16:33:54 +0530

    something_called_password

--- 21_project/password_manager.py
+++ 21_project/password_manager.py
@@ -1,11 +1,30 @@
+from cryptography.fernet import Fernet
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
 master_pwd = input("What is master password? ")
+key = load_key() +  master_pwd.encode()
+fer = Fernet(key)
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key","wb") as key_file:
+        key_file.write(key)
+'''
+
 
 def view():
     with open('password.txt','r') as f:
         for lines in f.readlines():
             data = lines.rstrip()
             user, passw = data.split("|")
-            print("User:" , user, "Password:",passw)
+            print("User:" , user, "| Password:", fer.decrypt(passw.encode()))
     
 def add():
     
@@ -13,7 +32,7 @@ def add():
     pwd =  input("Please enter the password: ")
     
     with open('password.txt','a') as f:
-        f.write(name+'|'+pwd+'\n')
+        f.write(name+'|'+ fer.encrypt(pwd.encode()).decode()+'\n')
     
 while True:
     mode = input("Which mode would you like to activate view/add or would you like to quit by pressing q: ").lower()
