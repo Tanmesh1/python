1b118e1cd0bdc34d064050db60ae1db6cbf846b2
Tanmesh Joshi <tanmeshj2002@gamil.com>
2026-01-29 13:26:54 +0530

    new_commits

--- study/DSA/binary_search.py
+++ study/DSA/binary_search.py
@@ -1,15 +1,36 @@
 # binary search is searching algorithm in sorted array 
 #  ###
 
-def binary(arr,n):
-    low = 0
-    high = len(arr) 
-    while(low <= high ):
-        mid = (low + high)//2
-        if arr[mid] == n:
-            return mid
-        elif arr[mid] < n:
-            low = mid +1
-        else:
-            high = mid - 1  
-    return -1
+# def binary(arr,n):
+#     low = 0
+#     high = len(arr) 
+#     while(low <= high ):
+#         mid = (low + high)//2
+#         if arr[mid] == n:
+#             return mid
+#         elif arr[mid] < n:
+#             low = mid +1
+#         else:
+#             high = mid - 1  
+#     return -1
+
+# # first occurence using binary search
+# def firstoccur(arr,n):
+#     low = 0
+#     high = len(arr)
+#     first = 0
+#     while(low <= high):
+#         mid = (low + high)//2
+#         if arr[mid] == n:
+#             first = arr[mid]
+#         elif arr[mid] < n:
+#             low = mid + 1
+#         else:
+#             high = mid - 1
+#     return first 
+
+# arr = [1,2,3,4,4,5,6,7]
+# n = 4 
+# print(firstoccur(arr,n))
+        
+print("hello World")
