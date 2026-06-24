e458e7da0c91556061bb8c4081e592097627a481
Tanmesh Joshi <tanmeshj2002@gamil.com>
2026-06-24 14:27:34 +0530

    aaa

---
+++ DSA/19_JUNE_2026.py
@@ -0,0 +1,19 @@
+# pattern 12
+
+for i in range(2,6):
+    j=1
+    while(j<i):
+        print(j,end="")
+        j=j+1
+    for k in range(6,1,-2):
+        print("*", end="")
+    for l in range(i,0,-1):
+        print(l, end="")
+    print()
+
+
+
+
+
+
+
